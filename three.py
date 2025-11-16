1da38c0cc332ba6a9ff826af1ffc0dfdbf413be7
Eric Gullufsen <eg628@cornell.edu>
2025-11-15 22:01:09 -0500

    catchup

--- three.py
+++ three.py
@@ -1,4 +1,10 @@
 #! /usr/bin/python3
 
+import re
+
+r2 = re.compile("[^A-Z][A-Z]{3}[a-z]{1}[A-Z]{3}[^A-Z]")
+
 f = open("3in.txt", "r")
-c = f.readlines()
+c = f.read()
+
+print("".join([x[4] for x in r2.findall(c)]))
